194619161f65625f1f4d40896a833f01e72a37b2
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-12 15:19:28 -0300

    Diferença entre codigos com identação

--- identacao_blocos/identacao_blocos.py
+++ identacao_blocos/identacao_blocos.py
@@ -1,8 +1,16 @@
-def sacar(valor):
+#idetação em blocos
+def sacar(valor): #apos os doispontos se inicia a identaçãp 4 linha para frente 
     saldo = 500
     
     if saldo >= valor:
-        print("valor sacado!")
+        print("valor sacado!") #identação 4 linhas a frente do if 
+        print("Retire seudinheiro na boca do caixa.")
 
+    print("Obrigado por ser nosso cliente, tenha um otimo dia!")    
 
-sacar(100)
+#codigo não identado
+#def depositar(valor):
+#saldo = 500
+#saldo += valor
+
+sacar(1000)
