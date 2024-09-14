d8efebc26d1af737ebd6276656072d6049a0eaca
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-14 01:37:50 -0300

    codigo comentando

--- estrutura_condicionais/estrutura_condicionais_aninhada.py
+++ estrutura_condicionais/estrutura_condicionais_aninhada.py
@@ -5,6 +5,8 @@ SALDO = 2000
 SAQUE = 1500
 CHEQUE_ESPECIAL = 450
 
+#Ambos os codigos precisam estar com a infomaçoes desejadas para saque realizado, saque cheque especial ou saldo insuficiente 
+#Roda esse bloco caso a conta_normal for True ea conta_universitaria for false 
 if CONTA_NORMAL:
     if SALDO >= SAQUE:
         print("Saque realizado com sucesso! ")
@@ -12,10 +14,14 @@ if CONTA_NORMAL:
         print("Saque realiazado com sucesso com o uso do cheque especial! ")
     else:
         print("Não foi possivel realizar o saque, saldo insuficiente!")
+
+#Roda essse bloco caso a conta_normal for False ea conta_universitaria for True
 elif CONTA_UNIVERSITARIA:
     if SALDO >= SAQUE:
         print("Saque realizado com sucesso! ")
     else:
         print("Saldo insuficiente")
+
+#Aparece na telacaso as condiçoes forem falsa falsa
 else:
     print("Sistema não reconheceu seu tipo de conta, entre em contato cm seu gerente.")
