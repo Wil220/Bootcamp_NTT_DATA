e13860e8c278a9e59decc1e40c10a61e63200713
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-13 23:58:44 -0300

    comentando o codigo

--- estrutura_condicionais/estrutura_condicionais.py
+++ estrutura_condicionais/estrutura_condicionais.py
@@ -1,6 +1,7 @@
 saldo = 2000.0
 saque = float(input("informe o valor do saque "))
 
+#condição simples onde se for verdaderia ele ira ler o if, caso for falsa ele pula o if e vai para o else!
 if saldo >= saque:
     print("Realizando saque! ")
 else:
