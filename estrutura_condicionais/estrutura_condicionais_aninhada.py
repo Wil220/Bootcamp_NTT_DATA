70ae76142f0576110c6e5458a7ba02e04503e7f1
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-14 01:26:28 -0300

    Teatnto o primeiro bloco sem interação(conta_normal)

---
+++ estrutura_condicionais/estrutura_condicionais_aninhada.py
@@ -0,0 +1,20 @@
+CONTA_NORMAL = True
+CONTA_UNIVERSITARIA = False
+
+SALDO = 2000
+SAQUE = 2500
+CHEQUE_ESPECIAL = 450
+
+if CONTA_NORMAL:
+    if SALDO >= SAQUE:
+        print("Saque realizado com sucesso! ")
+    elif  SAQUE <= (SALDO + CHEQUE_ESPECIAL):
+        print("Saque realiazado com sucesso com o uso do cheque especial! ")
+    else:
+        print("Não foi possivel realizar o saque, saldo insuficiente!")
+elif CONTA_UNIVERSITARIA:
+    if SALDO >= SAQUE:
+        print("Saque realizado com sucesso! ")
+    else:
+        print("Saldo insuficiente")
+
