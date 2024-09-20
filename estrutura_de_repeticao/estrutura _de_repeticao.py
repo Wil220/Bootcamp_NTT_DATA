d2a832a840ed6dfdfe192fdac26587125bdd190f
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-20 01:47:39 -0300

    iniciando uma tabuada simples com range

--- estrutura_de_repeticao/estrutura _de_repeticao.py
+++ estrutura_de_repeticao/estrutura _de_repeticao.py
@@ -1,4 +1,23 @@
-a = int(input("Informe um numero"))
+#a = int(input("Informe um numero: "))
+#print(a)
 
-a +=1
-print(a)
+#a +=1
+#print(a)
+
+#a +=1
+#print(a)
+
+# essa aplicação procura por alguma letra exemplo, se eu escrever python nas minhas vogais tem o O então eleaparece natela a letra O 
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+
+print() #Adiciona uma quebra delinha 
+
+#Taubuada
+
+for numero in range(0, 51, 5):
+    print(numero) 
