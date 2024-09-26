9423678b0a217e781be636e9e29f9a0a3d3a5b69
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-25 23:36:38 -0300

    metodos uteis

---
+++ metodos_uteis_classse_string/string_4.py
@@ -0,0 +1,15 @@
+nome = "Wilker"
+
+mensagem = f"""
+   Olá meu nome é {nome},
+Eu estou aprendendo python.
+   Essa mensagem tem diferentes recuos.
+"""
+print(mensagem) 
+
+print(
+    """
+ ==========  menu  ========== 
+1 - depositar
+2 - Sacar 
+0 - Sair  """)
