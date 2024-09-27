5c174821b253d82ee4432e351c5f174b5e1509ae
https://github.com/Wil220 <wilker.silva0714@gmail.com>
2024-09-26 23:39:49 -0300

    funções

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -1,2 +1,13 @@
-def ixibir_mensagem():
+def exibir_mensagem():
     print("Ola mundo!!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo(a) {nome}!")
+
+def exibir_mensagem_3(nome="Wilker"):
+    print(f"Seja bem vindo(a) {nome}")
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Guilherme")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="chapie")
